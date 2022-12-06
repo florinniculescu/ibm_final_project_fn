1b65f6b07c004a73c7815f027da92d253b170d6c
florin niculescu <florin.niculescu999@gmail.com>
2022-12-06 15:50:01 -0500

    views and urls

--- onlinecourse/views.py
+++ onlinecourse/views.py
@@ -152,7 +152,7 @@ def show_exam_result(request, course_id, submission_id):
             score = score + question.q_grade
         else:
             score = score
-
+    return render(request, 'onlinecourse/exam_result_bootstrap.html', context)
                 
                 
 
